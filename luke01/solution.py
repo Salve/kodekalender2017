8204591de231d1853c18ab6062606e6561ee82c2
Salve Spinnangr <salve.spinnangr@altibox.no>
2017-12-08 15:42:03 +0100

    Working solution

--- luke01/solution.py
+++ luke01/solution.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
+import sys
+from collections import Counter
+
+def create_ngram(word, n):
+    ngrams=[]
+    for pos, char in enumerate(word):
+        if (pos+n <= len(word)):
+            ngrams.append(word[pos:pos+n])
+    return ngrams
 
 words = []
 with open('wordlist.txt') as fp:
     for cnt, line in enumerate(fp):
-        words.append(line)
-        print(cnt)
+        words.append(line.strip())
 
-print(words[0], words[-1])
+word_to_match = Counter(sys.argv[1])
+max_n = int(sys.argv[2])
+
+#print(''.join(create_ngram(sys.argv[1], max_n)))
+
+for n in range(2,max_n+1):
+    print("Scanning with n={0}".format(n))
+    for word in words:
+        word_ngram = ''.join(create_ngram(word, n))
+        if (word_to_match == Counter(word_ngram)):
+            print("MATCH! {0}-{1}".format(n, word))
 
